ab5ad613564836fab11024d173516f6dac84bb98
derya <derya@aprilerp.com>
2023-12-05 22:21:22 +0300

    I started it startRecording func and stopRecording func but I cant make it stop

--- src/pages/NotLogin/NotLoginHomeScreen.js
+++ src/pages/NotLogin/NotLoginHomeScreen.js
@@ -6,19 +6,44 @@ import {MyContainer} from "../../components/Container/Container";
 import { MyButton } from "../../components/Common/Button/MyButton";
 import { StringContext } from "../../contexts/StringContext";
 import { RequestAllPermission } from "../../helper/functions/Permission";
+import AudioRecorderPlayer from 'react-native-audio-recorder-player';
 
 export const NotLoginHomeScreen = (props) => {
+    const audioRecorderPlayer = new AudioRecorderPlayer();
+
+    const [RecordSecs, setRecordSecs] = useState(0);
+    const [RecordTime, setRecordTime] = useState(0);
 
     const {myStrings} = useContext(StringContext);
 
     const [isRecording, setisRecording] = useState();
 
-    function _handleStartRecording() {
+    const _handleStartRecording = async = () => {
         
       RequestAllPermission()
         .then(() => {
-            console.log("HAZIRIM");
-            setisRecording(true)
+            audioRecorderPlayer.startRecorder().then((e) => {
+                console.log(e)
+                setisRecording(true)
+                audioRecorderPlayer.addRecordBackListener((res) => {
+                    console.log(res,"STARTTAN GELİYORUM");
+                    setRecordSecs(res.currentPosition);
+                    setRecordTime(audioRecorderPlayer.mmssss(Math.floor(res.currentPosition)));
+                })
+                setisRecording(true)
+            }).catch((error) => {
+                console.log(error);
+            })
+
+            // console.log(result, "START")
+
+            // audioRecorderPlayer.addRecordBackListener((e) => {
+
+            //     setRecordSecs(e.currentPosition);
+            //     setRecordTime(audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)));
+            // })
+
+            // setisRecording(true)
         })
         .catch((error) => {
             console.log("error", error)
@@ -26,9 +51,31 @@ export const NotLoginHomeScreen = (props) => {
         
     }
 
-    function _handleStopRecording() {
-        setisRecording(false);
+    const _handleStopRecording = () => {
+
+        audioRecorderPlayer.stopRecorder().then((e) => {
+            console.log(e)
+            setRecordTime(0);
+            setRecordSecs(0);
+        }).catch((error) => {
+            console.log(error);
+        })
+
+        // const result = audioRecorderPlayer.stopRecorder();
+
+        // console.log(result, "STOP")
+
+        // audioRecorderPlayer.removeRecordBackListener(() => {
+        //     setRecordSecs();
+        //     setRecordTime(audioRecorderPlayer.mmssss(0));
+        // })
+        
+        // setisRecording(false);
     }
+
+    useEffect(() => {
+        console.log(RecordTime);
+    }, [RecordTime])
 
     return (
         <MyContainer
@@ -41,9 +88,9 @@ export const NotLoginHomeScreen = (props) => {
             <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
                 
                 {!isRecording ?
-                    <MyButton size="big" buttonText={myStrings.button.start} onPress={() => _handleStartRecording()} /> :
+                    <MyButton size="big" buttonText={myStrings.button.start} onPress={_handleStartRecording} /> :
 
-                    <MyButton size="big" buttonText={myStrings.button.stop} onPress={() => _handleStopRecording()} />
+                    <MyButton size="big" buttonText={myStrings.button.stop} onPress={_handleStopRecording} />
                 }
             </View>
 
